76fbfd57e69e5bec0cc0c5cb8ab9efaf627f2f50
luoyao <luoyao@luoyao.internet.com>
2020-09-10 21:40:44 +0800

    `

--- src/main/java/com/ei41/base/configuration/DataSourceConfiguration.java
+++ src/main/java/com/ei41/base/configuration/DataSourceConfiguration.java
@@ -6,10 +6,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.alibaba.druid.pool.DruidDataSource;
 
 @Configuration
+@EnableTransactionManagement
 @MapperScan(basePackages = "com.ei41.*.mapper")
 public class DataSourceConfiguration {
 
